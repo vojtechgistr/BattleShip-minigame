0cf36e230e0b06eaf283ed03245d09516c960b12
Vojtěch Gistr <vojtech.gistr@email.cz>
2022-09-03 15:15:16 +0200

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,16 @@
 #include "main.h"
 
 
-int main() {
+int main()
+{
+    // Get the console handle
+    HWND console = GetConsoleWindow();
+
+    // Set window size
+    // MoveWindow(window_handle, x, y, width, height, redraw_window);
+    MoveWindow(console, 100, 100, 992, 548, TRUE);
+
+    // Start game
     BattleshipMinigame game;
     game.start();
 
